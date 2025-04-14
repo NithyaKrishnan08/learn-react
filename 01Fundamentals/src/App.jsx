a7a2bd0c492b80462ee89936acaef435e6b7bc43
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-04-14 10:44:02 -0400

    props and props destructuring

--- 01Fundamentals/src/App.jsx
+++ 01Fundamentals/src/App.jsx
@@ -1,12 +1,23 @@
-import UserList from './components/UserList.jsx';
-import ProductList from './components/ProductList.jsx';
-function App () {
-  return(
+import Card from "./components/Card";
+
+const App = () => {
+  return (
     <div>
-      <UserList />
-      <ProductList />
+      <h1>My Cards</h1>
+      <Card>
+        <h2>Card Title 1</h2>
+        <p>This is some content for card 1.</p>
+      </Card>
+      <Card>
+        <h2>Card Title 2</h2>
+        <p>This is some content for card 2.</p>
+      </Card>
+      <Card>
+        <h2>Card Title 3</h2>
+        <p>This is some content for card 3.</p>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
