fa3f421d15dd0b0461ceb9c49f3e238ef00c9201
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-04-15 14:33:11 -0400

    React Portal

--- 01Fundamentals/src/App.jsx
+++ 01Fundamentals/src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import TodoList from "./components/TodoList";
-import Profile from "./components/Profile";
-import Counter from "./components/Counter";
-import ShoppingList from "./components/ShoppingList";
+import CopyInput from "./components/CopyInput";
+
 function App() {
   return (
     <div>
-      <h1>React useState Examples</h1>
-      <Counter />
-      <TodoList />
-      <Profile />
-      <ShoppingList />
+      <h1>React Portal</h1>
+      <CopyInput />
     </div>
   );
 }
